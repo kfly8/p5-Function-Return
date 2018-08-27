9b271cffaa7d6c91ca4e822109eaa646fd7bd32e
Kenta, Kobayashi <kentafly88@gmail.com>
2018-08-27 22:54:22 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Function::Return - It's new $module
+
+# SYNOPSIS
+
+    use Function::Return;
+
+# DESCRIPTION
+
+Function::Return is ...
+
+# LICENSE
+
+Copyright (C) Kenta, Kobayashi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kenta, Kobayashi <kentafly88@gmail.com>
---
+++ lib/Function/Return.pm
@@ -0,0 +1,39 @@
+package Function::Return;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Function::Return - It's new $module
+
+=head1 SYNOPSIS
+
+    use Function::Return;
+
+=head1 DESCRIPTION
+
+Function::Return is ...
+
+=head1 LICENSE
+
+Copyright (C) Kenta, Kobayashi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kenta, Kobayashi E<lt>kentafly88@gmail.comE<gt>
+
+=cut
+
